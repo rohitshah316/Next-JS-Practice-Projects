fd18bf289d77ef4c88c1bbe498bea37a4f1fefed
Rohit Shah <shahrohit2223@gmail.com>
2026-05-26 21:38:51 +0545

    GET METHOD

---
+++ 07-api-routes/src/app/api/users/[id]/route.js
@@ -0,0 +1,20 @@
+ const users=[
+        {id:1,name:"Rohit"},
+        {id:2,name:"Aniruddha"},
+        {id:3,name:"Prinsa"}
+    ];
+
+export async function GET(request,{params}){
+    const {id}=await params;
+
+    const user=users.find(u=>u.id===parseInt(id));
+
+    if(!user){
+        return Response.json(
+            {message:"User not Found"},
+            {status:404}
+        )
+    }
+
+    return Response.json(user);
+}
--- 07-api-routes/src/app/api/users/route.js
+++ 07-api-routes/src/app/api/users/route.js
@@ -2,6 +2,7 @@ export async function GET(){
     const users=[
         {id:1,name:"Rohit"},
         {id:2,name:"Aniruddha"},
+        {id:3,name:"Prinsa"}
     ]
 
     return Response.json(users);
@@ -10,8 +11,10 @@ export async function GET(){
 export async function POST(request){
     const body=await request.json();
 
+    console.log(body)
+
     return Response.json({
         message:"User Created Successfully",
-        user:body,
+        newUser:body,
     });
 }
