a2849e0571ff3f92b7cfa1590b82a2fbad7f8115
Rohit Shah <shahrohit2223@gmail.com>
2026-06-13 19:39:40 +0545

    08 server vs client demo

--- 08-server-vs-client-demo/src/app/page.js
+++ 08-server-vs-client-demo/src/app/page.js
@@ -1,10 +1,18 @@
-import UserList from "@/app/components/UserList";
-
-
+'use client'
+import UserList from "./components/UserList";
+import {useState} from 'react'
 export default function HomePage(){
+  const [count,setCount]=useState(0);
+
+  console.log("HomePage Running")
+
   return(
-    <main className="p-10">
-      <h1 className="text-3xl font-bold mb-6">Server vs client components</h1>
+    <main>
+      <h1>Server vs Client Components</h1>
+      <p>Count:{count}</p>
+      <button className="bg-gray-500 p-2 rounded-2xl m-10"
+      onClick={()=>setCount(c=>c+1)}
+      >Count:{count}</button>
       <UserList/>
     </main>
   )
